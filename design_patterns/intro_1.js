1d7009467fc9b620e575dff824cfe5be3a04056a
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-11-09 17:10:18 -0500

    add design_patterns chapter 1 notes

---
+++ design_patterns/intro_1.js
@@ -0,0 +1,20 @@
+// Three Design Pattern Types: Design Patterns, Coding Patterns, and
+// Antipatterns
+
+// 1. Design Patterns: Focuses on JS implementations, as some design patterns
+//    are not as relevant to JS (since it is a dynamic, not strongly typed,
+//    programming language). Most of this material is recycled from "Design
+//    Patterns: Elements of Reusable Object-Oriented Software" -- notably
+//    composition is preferred over class-inheritence here (p.4).  Examples:
+//    singleton, factory, decorator, observer.
+
+// 2. Coding Patterns: The MAIN TOPIC of this book -- "good practices related to
+//    the unique features of the language"
+
+// 3. Antipatterns: Common techniques/approaches that end up causing more
+//    problems than solutions.
+
+// 4. Strict Mode: You should get into the habit of using "use strict" -- strict
+//    mode, which "removes features from the language, making programs simpler
+//    and less error-prone" -- note that running JSLint in the background will
+//    also enforce strict mode compliance
